19347f0d17edc8d1e622ed727bf83983d6cc2c4b
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2020-05-01 15:57:36 +0800

    Update and rename Predict_Dog.py to Cell_Predic.py

--- Cell_Predic.py
+++ Cell_Predic.py
@@ -7,14 +7,15 @@ import numpy as np
 from PIL import Image
 
 class Prediction(object):
-    def __init__(self,ModelFile,PredictFile,DogType,Width=100,Height=100):
+    def __init__(self,ModelFile,PredictFile,CellType,Width=100,Height=100):
         self.modelfile = ModelFile
         self.predict_file = PredictFile
         self.Width = Width
         self.Height = Height
-        self.DogType = DogType
+        self.CellType = CellType
 
     def Predict(self):
+        #加载预测照片并且将其处理成100*100大小的图像
         model = load_model(self.modelfile)
         img_open = Image.open(self.predict_file)
         conv_RGB = img_open.convert('RGB')
@@ -32,23 +33,25 @@ class Prediction(object):
         Final_prediction = [result.argmax() for result in prediction][0]
         print(Final_prediction)
 
-        #延伸教程
+        #打印出预测结果
         count = 0
         for i in prediction[0]:
             print(i)
-            percentage = '%.2f%%' % (i * 100)
+            percentage = '%3f%%' % (i * 100)
             print(self.DogType[count],'概率:' ,percentage)
             count +=1
 
-
+        #展示导入的预测图象
     def ShowPredImg(self):
         image = processimage.imread(self.predict_file)
         plt.imshow(image)
         plt.show()
 
 
-DogType = ['哈士奇', '德国牧羊犬', '拉布拉多', '萨摩耶犬']
-Pred = Prediction(PredictFile='2.jpg',ModelFile='dogfinder.h5',Width=100,Height=100,DogType=DogType)
+CellType = ['嗜酸性粒细胞','嗜碱性粒细胞','中性粒细胞'，'空']
+
+#其中PredictFile='2.jpg'为需要识别的图像,ModelFile='cellfinder.h5'为之前训练好的模型文件,CellType为上面几种细胞分类集中的种类列表
+Pred = Prediction(PredictFile='2.jpg',ModelFile='cellfinder.h5',Width=100,Height=100,CellType=CellType)
 Pred.Predict()
 
 
